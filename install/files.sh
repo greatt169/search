90dec822e472750561adac7116143d875867d6b8
Mikhail Dadaev <dadaev@aeroidea.ru>
2019-04-30 19:29:34 +0300

    UPD ENV

--- install/files.sh
+++ install/files.sh
@@ -1,2 +1,6 @@
-docker-compose exec php-fpm wget -c https://aeroidea.ru/upload/backend/bitrix.tar.gz -P ./public
+docker-compose exec php-fpm wget \
+		--user loginarea \
+		--password passarea \
+		-c https://aeroidea.ru/upload/backend/secure/bitrix.tar.gz \
+		-P ./public
 docker-compose exec php-fpm tar -xvf public/bitrix.tar.gz -C ./public
