981c38a19c33b2206c0595c259d5d22ea60f27c7
mikl <greatt169.92@mail.ru>
2019-05-02 18:45:03 +0300

    add data

---
+++ public/data.php
@@ -0,0 +1,23 @@
+<?php
+return [
+    [
+        'id' => 1,
+        'brand' => 'BMW',
+        'model' => 'X5',
+        'year' => 2005,
+        'price' => 10000,
+    ],
+    [
+        'id' => 2,
+        'brand' => 'Lada',
+        'model' => 'Granta',
+        'year' => 2017,
+        'price' => 4000,
+    ],[
+        'id' => 3,
+        'brand' => 'Volkswagen',
+        'model' => 'Polo',
+        'year' => 2015,
+        'price' => 50000,
+    ]
+];
