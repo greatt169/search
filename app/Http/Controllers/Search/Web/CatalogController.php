9260088027b22036d804901abbf0b7ae0c876eac
Mikhail Dadaev <dadaev@aeroidea.ru>
2019-09-19 15:22:55 +0300

    started catalog integration

--- app/Http/Controllers/Search/Web/CatalogController.php
+++ app/Http/Controllers/Search/Web/CatalogController.php
@@ -8,6 +8,24 @@ class CatalogController extends Controller
 {
    public function index()
    {
+       $apiInstance = new \SwaggerSearch\Api\CatalogApi(
+       // If you want use custom http client, pass your client which implements `GuzzleHttp\ClientInterface`.
+       // This is optional, `GuzzleHttp\Client` will be used as default.
+           new \GuzzleHttp\Client()
+       );
+       $apiInstance->getConfig()->setHost('http://10.101.2.10/api');
+       $engine = 'elasticsearch'; //  | Код поискового движка
+       $index = 'auto'; //  | Код индекса
+       $request = new \SwaggerSearch\Model\Request([]); // \SwaggerSearch\Model\Request |
+       $page = 1; // int | Номер запрашиваемой страницы результата
+       $page_size = 20; // int | Количество возвращаемых объектов на странице
+
+       try {
+           $result = $apiInstance->engineIndexIndexCatalogSearchPost($engine, $index, $request, $page, $page_size);
+           dump($result);
+       } catch (\Exception $e) {
+           echo 'Exception when calling CatalogApi->engineIndexIndexCatalogSearchPost: ', $e->getMessage(), PHP_EOL;
+       }
        return view('demo.catalog');
    }
 }
