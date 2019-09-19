f775308d30d5f67c902a70b3b52b688385eae57a
mikl <greatt169.92@mail.ru>
2019-09-19 09:19:13 +0300

    updated layouts

--- resources/views/demo/layouts/app.blade.php
+++ resources/views/demo/layouts/app.blade.php
@@ -67,157 +67,18 @@
 </header> <!-- section-header.// -->
 
 
-<!-- ========================= SECTION CONTENT ========================= -->
-<section class="section-content bg padding-y">
-    <div class="container">
 
-        <div class="card">
-            <div class="card-body">
-                <div class="row">
-                    <div class="col-md-3-24"><strong>Вы здесь:</strong></div> <!-- col.// -->
-                    <nav class="col-md-18-24">
-                        <ol class="breadcrumb">
-                            <li class="breadcrumb-item"><a href="#">Главная</a></li>
-                            <li class="breadcrumb-item"><a href="#">Фильтрация</a></li>
-                        </ol>
-                    </nav> <!-- col.// -->
-                </div> <!-- row.// -->
-            </div> <!-- col.// -->
-        </div> <!-- row.// -->
-
-
-        <section class="section-pagetop">
-            <h2 class="title-doc">Каталог продукции</h2>
-        </section>
-
-        <div class="row">
-            <aside class="col-sm-3">
-
-                <div class="card card-filter">
-                    <article class="card-group-item">
-                        <header class="card-header">
-                            <a class="" aria-expanded="true" href="#" data-toggle="collapse" data-target="#collapse22">
-                                <i class="icon-action fa fa-chevron-down"></i>
-                                <h6 class="title">By Category</h6>
-                            </a>
-                        </header>
-                        <div style="" class="filter-content collapse show" id="collapse22">
-                            <div class="card-body">
-                                <form class="pb-3">
-                                    <div class="input-group">
-                                        <input class="form-control" placeholder="Search" type="text">
-                                        <div class="input-group-append">
-                                            <button class="btn btn-primary" type="button"><i class="fa fa-search"></i>
-                                            </button>
-                                        </div>
-                                    </div>
-                                </form>
-
-                                <ul class="list-unstyled list-lg">
-                                    <li><a href="#">Cras justo odio <span
-                                                    class="float-right badge badge-light round">142</span>
-                                        </a></li>
-                                    <li><a href="#">Dapibus ac facilisis <span
-                                                    class="float-right badge badge-light round">3</span> </a></li>
-                                    <li><a href="#">Morbi leo risus <span
-                                                    class="float-right badge badge-light round">32</span> </a></li>
-                                    <li><a href="#">Another item <span
-                                                    class="float-right badge badge-light round">12</span> </a></li>
-                                </ul>
-                            </div> <!-- card-body.// -->
-                        </div> <!-- collapse .// -->
-                    </article> <!-- card-group-item.// -->
-                    <article class="card-group-item">
-                        <header class="card-header">
-                            <a href="#" data-toggle="collapse" data-target="#collapse33">
-                                <i class="icon-action fa fa-chevron-down"></i>
-                                <h6 class="title">By Price </h6>
-                            </a>
-                        </header>
-                        <div class="filter-content collapse show" id="collapse33">
-                            <div class="card-body">
-                                <input type="range" class="custom-range" min="0" max="100" name="">
-                                <div class="form-row">
-                                    <div class="form-group col-md-6">
-                                        <label>Min</label>
-                                        <input class="form-control" placeholder="$0" type="number">
-                                    </div>
-                                    <div class="form-group text-right col-md-6">
-                                        <label>Max</label>
-                                        <input class="form-control" placeholder="$1,0000" type="number">
-                                    </div>
-                                </div> <!-- form-row.// -->
-                                <button class="btn btn-block btn-outline-primary">Apply</button>
-                            </div> <!-- card-body.// -->
-                        </div> <!-- collapse .// -->
-                    </article> <!-- card-group-item.// -->
-                    <article class="card-group-item">
-                        <header class="card-header">
-                            <a href="#" data-toggle="collapse" data-target="#collapse44">
-                                <i class="icon-action fa fa-chevron-down"></i>
-                                <h6 class="title">By Feature </h6>
-                            </a>
-                        </header>
-                        <div class="filter-content collapse show" id="collapse44">
-                            <div class="card-body">
-                                <form>
-                                    <label class="form-check">
-                                        <input class="form-check-input" value="" type="checkbox">
-                                        <span class="form-check-label">
-				  	<span class="float-right badge badge-light round">5</span>
-				    Samsung
-				  </span>
-                                    </label>  <!-- form-check.// -->
-                                    <label class="form-check">
-                                        <input class="form-check-input" value="" type="checkbox">
-                                        <span class="form-check-label">
-				  	<span class="float-right badge badge-light round">13</span>
-				    Mersedes Benz
-				  </span>
-                                    </label> <!-- form-check.// -->
-                                    <label class="form-check">
-                                        <input class="form-check-input" value="" type="checkbox">
-                                        <span class="form-check-label">
-				  	<span class="float-right badge badge-light round">12</span>
-				    Nissan Altima
-				  </span>
-                                    </label>  <!-- form-check.// -->
-                                    <label class="form-check">
-                                        <input class="form-check-input" value="" type="checkbox">
-                                        <span class="form-check-label">
-				  	<span class="float-right badge badge-light round">32</span>
-				    Another Brand
-				  </span>
-                                    </label>  <!-- form-check.// -->
-                                </form>
-                            </div> <!-- card-body.// -->
-                        </div> <!-- collapse .// -->
-                    </article> <!-- card-group-item.// -->
-                </div> <!-- card.// -->
-
-
-            </aside> <!-- col.// -->
-            <main class="col-sm-9">
-
-
-            </main> <!-- col.// -->
-        </div>
-
-    </div> <!-- container .//  -->
-</section>
-<!-- ========================= SECTION CONTENT END// ========================= -->
 
 <!-- ========================= FOOTER ========================= -->
 <footer class="section-footer bg-secondary">
     <div class="container">
         <section class="footer-bottom row border-top-white">
             <div class="col-sm-6">
-                <p class="text-white-50"> Made with <3 <br> by Vosidiy M.</p>
+                <p class="text-white-50"> Дизайн и разработка<br> A E R O</p>
             </div>
             <div class="col-sm-6 text-right">
                 <p class="text-sm-right text-white-50">
-                    Copyright &copy 2018 <br>
-                    <a href="http://bootstrap-ecommerce.com" class="text-white-50">Bootstrap-ecommerce UI kit</a>
+                    Все права защищены &copy @php echo date('Y')@endphp <br>
                 </p>
             </div>
         </section> <!-- //footer-top -->
