ee34342ec45305e91a865e9942a4b8e23c1c7f76
mikl <greatt169.92@mail.ru>
2019-09-26 07:46:52 +0300

    start debugging

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -2,10 +2,53 @@
 
 namespace App\Http\Controllers;
 
+use App\Exceptions\ApiException;
+use App\Search\Index\Listeners\SourceListener;
+use App\Search\Index\Source\Elasticsearch;
+use App\Search\UseCases\Errors\Error;
+use JsonStreamingParser\Parser;
+
 class TestController extends Controller
 {
    public function index()
    {
+
+
+       $listener = new SourceListener(function ($rawItems) {
+           $source = new Elasticsearch('auto', '/var/www/public/data.json', '/var/www/public/settings.json');
+           $items = $source->getElementsForIndexing($rawItems);
+           $params = ['body' => []];
+           foreach ($items as $index => $document) {
+               $i = $index + 1;
+               $arDocAttributes = [];
+               foreach ($document['attributes'] as $attributeCode => $attributeValue) {
+                   $arDocAttributes[$attributeCode] = $attributeValue;
+               }
+               $arDocAttributes['raw_data'] = $document['raw_data'];
+               $arDocAttributes['search_data'] = $document['search_data'];
+               $params['body'][] = [
+                   'index' => [
+                       '_id' => $document['id']
+                   ]
+               ];
+               $params['body'][] = $arDocAttributes;
+               dump($params);
+           }
+       });
+
+       $source = new Elasticsearch('auto', '/var/www/public/data.json', '/var/www/public/settings.json');
+       $listener->setBatchSize(100);
+       $stream = fopen($source->getDataLink(), 'r');
+       try {
+           $parser = new Parser($stream, $listener);
+           $parser->parse();
+           $total = $listener->getTotal();
+           fclose($stream);
+       } catch (\Exception $e) {
+           fclose($stream);
+       }
+
+
        return 'test';
    }
 }
